326cdc5328896c1b3a3680fad3943c3ea131f125
jesus-manuel-navarro <animalillo44@gmail.com>
2024-05-06 07:49:48 -0700

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Jesus on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
