2b8f7ce4404bdb385cda63116fbc9600b75f5991
jesus-manuel-navarro <animalillo44@gmail.com>
2024-05-06 09:22:22 -0700

    con ejemplo de buttonstyle

--- Calculadora/ContentView.swift
+++ Calculadora/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             ExtractedView()
@@ -28,7 +29,8 @@ struct ExtractedView: View {
                 }, label: {
                     Text("1")
                 }
-                )  // fin de boton
+                )
+               // fin de boton
                 Button(action: {
                     print("2")
                 }, label: {
@@ -85,3 +87,22 @@ struct ExtractedView: View {
         }
     }
 }
+/*
+ struct BlueButton: ButtonStyle {
+     func makeBody(configuration: Configuration) -> some View {
+         configuration.label
+             .padding()
+             .background(Color(red: 0, green: 0, blue: 0.5))
+             .foregroundStyle(.white)
+             .clipShape(Capsule())
+     }
+ }
+
+ struct ContentView: View {
+     var body: some View {
+         Button("Press Me") {
+             print("Button pressed!")
+         }
+         .buttonStyle(BlueButton())
+     }
+ } */
